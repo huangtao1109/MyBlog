bfbaf5f1ca1487b61b955528c013b62a42ae2af4
huangtao1109 <huangtao1109@foxmail.com>
2020-03-29 15:18:57 +0800

    Initial commit

---
+++ article/models.py
@@ -0,0 +1,70 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from django.urls import reverse
+# Django-taggit
+from taggit.managers import TaggableManager
+from PIL import Image
+
+
+class ArticleColumn(models.Model):
+    """
+    栏目的 Model
+    """
+    # 栏目标题
+    title = models.CharField(max_length=100, blank=True)
+    # 创建时间
+    created = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
+
+
+class ArticlePost(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    created = models.DateTimeField(default=timezone.now)
+    updated = models.DateTimeField(auto_now=True)
+    total_views = models.PositiveIntegerField(default=0)
+    # 文章标签
+    tags = TaggableManager(blank=True)
+    # 文章标题图
+    avatar = models.ImageField(upload_to='article/%Y%m%d/', blank=True)
+    # 点赞数统计
+    likes = models.PositiveIntegerField(default=0)
+
+    # 文章栏目的 “一对多” 外键
+    column = models.ForeignKey(
+        ArticleColumn,
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+        related_name='article'
+    )
+
+    # 获取文章地址
+    def get_absolute_url(self):
+        return reverse('article:article_detail', args=[self.id])
+
+    # 保存时处理图片
+    def save(self, *args, **kwargs):
+        # 调用原有的 save() 的功能
+        article = super(ArticlePost, self).save(*args, **kwargs)
+
+        # 固定宽度缩放图片大小
+        if self.avatar and not kwargs.get('update_fields'):
+            image = Image.open(self.avatar)
+            (x, y) = image.size
+            new_x = 400
+            new_y = int(new_x * (y / x))
+            resized_image = image.resize((new_x, new_y), Image.ANTIALIAS)
+            resized_image.save(self.avatar.path)
+
+        return article
+
+    class Meta:
+        ordering = ('-created',)
+
+    def __str__(self):
+        return self.title
---
+++ article/views.py
@@ -0,0 +1,247 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
+
+from .models import ArticlePost
+import markdown
+# 引入redirect重定向模块
+from django.shortcuts import render, redirect
+# 引入HttpResponse
+from django.http import HttpResponse
+# 引入刚才定义的ArticlePostForm表单类
+from .forms import ArticlePostForm
+# 引入User模型
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+# 引入分页模块
+from django.core.paginator import Paginator
+# 引入 Q 对象
+from django.db.models import Q
+from comment.models import Comment
+from .models import ArticleColumn
+# 引入评论表单
+from comment.forms import CommentForm
+
+from django.shortcuts import get_object_or_404
+
+
+# def article_list(request):
+#     article_list = ArticlePost.objects.all()
+#     # 每页显示 6 篇文章
+#     paginator = Paginator(article_list, 6)
+#     # 获取 url 中的页码
+#     page = request.GET.get('page')
+#     # 将导航对象相应的页码内容返回给 articles
+#     articles = paginator.get_page(page)
+#
+#     context = {'articles': articles}
+#     return render(request, 'article/list.html', context)
+
+
+# 加入根据浏览量对最热文章排序，因此重写文章列表函数
+# 加入搜索功能
+def article_list(request):
+    # 从 url 中提取查询参数
+    search = request.GET.get('search')
+    order = request.GET.get('order')
+    column = request.GET.get('column')
+    tag = request.GET.get('tag')
+
+    # 初始化查询集
+    article_list = ArticlePost.objects.all()
+
+    # 搜索查询集
+    if search:
+        # 用 Q对象 进行联合搜索
+        article_list = article_list.filter(
+            Q(title__icontains=search) |
+            Q(body__icontains=search)
+        )
+    else:
+        # 将 search 参数重置为空
+        search = ''
+
+    # 栏目查询集
+    if column is not None and column.isdigit():
+        article_list = article_list.filter(column=column)
+
+    # 标签查询集
+    if tag and tag != 'None':
+        article_list = article_list.filter(tags__name__in=[tag])
+
+    # 查询集排序
+    if order == 'total_views':
+        article_list = article_list.order_by('-total_views')
+
+    paginator = Paginator(article_list, 3)
+    page = request.GET.get('page')
+    articles = paginator.get_page(page)
+
+    # 需要传递给模板（templates）的对象
+    context = {
+        'articles': articles,
+        'order': order,
+        'search': search,
+        'column': column,
+        'tag': tag,
+    }
+
+    return render(request, 'article/list.html', context)
+
+
+def article_detail(request, id):
+    # article = ArticlePost.objects.get(id=id)
+    article = get_object_or_404(ArticlePost, id=id)
+    # 取出文章评论
+    comments = Comment.objects.filter(article=id)
+
+    # 浏览量 +1
+    article.total_views += 1
+    article.save(update_fields=['total_views'])
+
+    # 修改 Markdown 语法渲染
+    md = markdown.Markdown(
+        extensions=[
+            # 包含 缩写、表格等常用扩展
+            'markdown.extensions.extra',
+            # 语法高亮扩展
+            'markdown.extensions.codehilite',
+            # 目录扩展
+            'markdown.extensions.toc',
+        ]
+    )
+    article.body = md.convert(article.body)
+
+    # 新增了md.toc对象
+    # 添加comments上下文
+    # 引入评论表单
+    comment_form = CommentForm()
+    context = {'article': article, 'toc': md.toc, 'comments': comments, 'comment_form': comment_form}
+
+    return render(request, 'article/detail.html', context)
+
+
+# 检查登录
+@login_required(login_url='/userprofile/login/')
+# 写文章的视图
+def article_create(request):
+    # 判断用户是否提交数据
+    if request.method == "POST":
+        # 将提交的数据赋值到表单实例中
+        # 增加了 request.FILES
+        article_post_form = ArticlePostForm(request.POST, request.FILES)
+        # 判断提交的数据是否满足模型的要求
+        if article_post_form.is_valid():
+            # 保存数据，但暂时不提交到数据库中
+            new_article = article_post_form.save(commit=False)
+            # 指定数据库中 id=1 的用户为作者
+            # new_article.author = User.objects.get(id=1)
+            # 指定目前登录的用户为作者
+            new_article.author = User.objects.get(id=request.user.id)
+            if request.POST['column'] != 'none':
+                new_article.column = ArticleColumn.objects.get(id=request.POST['column'])
+            # 将新文章保存到数据库中
+            new_article.save()
+            # 保存 tags 的多对多关系
+            article_post_form.save_m2m()
+            # 完成后返回到文章列表
+            return redirect("article:article_list")
+        # 如果数据不合法，返回错误信息
+        else:
+            return HttpResponse("表单内容有误，请重新填写。")
+    # 如果用户请求获取数据
+    else:
+        columns = ArticleColumn.objects.all()
+        # 创建表单类实例
+        article_post_form = ArticlePostForm()
+        # 赋值上下文
+        context = {'article_post_form': article_post_form, 'columns': columns}
+        # 返回模板
+        return render(request, 'article/create.html', context)
+
+
+# 提醒用户登录
+@login_required(login_url='/userprofile/login/')
+# 删文章
+def article_delete(request, id):
+    # 根据 id 获取需要删除的文章
+    article = ArticlePost.objects.get(id=id)
+    # 调用.delete()方法删除文章
+    article.delete()
+    # 完成删除后返回文章列表
+    return redirect("article:article_list")
+
+
+# 提醒用户登录
+@login_required(login_url='/userprofile/login/')
+# 安全删除文章
+def article_safe_delete(request, id):
+    if request.method == 'POST':
+        article = ArticlePost.objects.get(id=id)
+        # 过滤非作者的用户
+        if request.user != article.author:
+            return HttpResponse("没有权限！")
+        article.delete()
+        return redirect("article:article_list")
+    else:
+        return HttpResponse("仅允许post请求！")
+
+
+# 提醒用户登录
+@login_required(login_url='/userprofile/login/')
+# 更新文章
+def article_update(request, id):
+    """
+    更新文章的视图函数
+    通过POST方法提交表单，更新titile、body字段
+    GET方法进入初始表单页面
+    id： 文章的 id
+    """
+
+    # 获取需要修改的具体文章对象
+    article = ArticlePost.objects.get(id=id)
+    # 过滤非作者的用户
+    if request.user != article.author:
+        return HttpResponse("没有权限！")
+    # 判断用户是否为 POST 提交表单数据
+    if request.method == "POST":
+        # 将提交的数据赋值到表单实例中
+        article_post_form = ArticlePostForm(data=request.POST)
+        # 判断提交的数据是否满足模型的要求
+        if article_post_form.is_valid():
+            # 保存新写入的 title、body 数据并保存
+            article.title = request.POST['title']
+            article.body = request.POST['body']
+            if request.POST['column'] != 'none':
+                article.column = ArticleColumn.objects.get(id=request.POST['column'])
+            else:
+                article.column = None
+            article.save()
+            # 完成后返回到修改后的文章中。需传入文章的 id 值
+            return redirect("article:article_detail", id=id)
+        # 如果数据不合法，返回错误信息
+        else:
+            return HttpResponse("表单内容有误，请重新填写。")
+
+    # 如果用户 GET 请求获取数据
+    else:
+        columns = ArticleColumn.objects.all()
+        # 创建表单类实例
+        article_post_form = ArticlePostForm()
+        # 赋值上下文，将 article 文章对象也传递进去，以便提取旧的内容
+        context = {
+            'article': article,
+            'article_post_form': article_post_form,
+            'columns': columns,
+        }
+        # 将响应返回到模板中
+        return render(request, 'article/update.html', context)
+
+
+# 点赞数 +1
+class IncreaseLikesView(View):
+    def post(self, request, *args, **kwargs):
+        article = ArticlePost.objects.get(id=kwargs.get('id'))
+        article.likes += 1
+        article.save()
+        return HttpResponse('success')
